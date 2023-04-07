24ca70487d1d07304be6c5e88b29ff5d42abe2de
rtzeg <korolt073@gmail.com>
2023-04-07 14:08:38 +0500

    fixed nazif's code

--- modules/actorId.js
+++ modules/actorId.js
@@ -1,7 +1,16 @@
 import { getData } from "/modules/http.request";
+
 const actor_id = location.search.split("=").at(-1);
-let img = document.querySelector('img')
-getData(`person/${actor_id}/popular`).then((res) => {
-   img.src = `${import.meta.env.VITE_IMG_URL}${item.profile_path}`
-   
+let img_cont = document.querySelector('.actor-info__left')
+
+getData(`person/${actor_id}/tagged_images`)
+.then((res) => {
+   console.log(res);
+   for(let item of res.data.results) {
+      let img = document.createElement('img')
+
+      img.src = `${import.meta.env.VITE_IMG_URL}${item.file_path}`
+
+      img_cont.append(img)
+   }  
 });
